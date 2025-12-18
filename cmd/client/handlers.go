23543a2fdf66788f5b73d8f3405badca1468169e
ManoloEsS <manolo.estrada.s@gmail.com>
2025-12-17 22:43:14 -0700

    feat(client): Add acknowledgement functionality for pause and move messages
    -Add AckType enum with Ack, NackReque, and NackDiscard options
    -Update handler and consumer to support message acknowledgement based on
    processing outcome

--- cmd/client/handlers.go
+++ cmd/client/handlers.go
@@ -4,21 +4,34 @@ import (
 	"fmt"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 )
 
 // Handler for pause/resume messages from the direct pause exchange
-func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
-	return func(playingState routing.PlayingState) {
+func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) pubsub.AckType {
+	return func(playingState routing.PlayingState) pubsub.AckType {
 		defer fmt.Print("> ")
 		gs.HandlePause(playingState)
+		return pubsub.Ack
 	}
 }
 
 // Handler for player's army move messages from the topic exchange
-func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
-	return func(move gamelogic.ArmyMove) {
+func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) pubsub.AckType {
+	return func(move gamelogic.ArmyMove) pubsub.AckType {
 		defer fmt.Print("> ")
-		gs.HandleMove(move)
+		moveOutcome := gs.HandleMove(move)
+		switch moveOutcome {
+		case gamelogic.MoveOutComeSafe:
+			return pubsub.Ack
+		case gamelogic.MoveOutcomeMakeWar:
+			return pubsub.Ack
+		case gamelogic.MoveOutcomeSamePlayer:
+			return pubsub.NackDiscard
+		default:
+			fmt.Println("error: unknown move outcome")
+			return pubsub.NackDiscard
+		}
 	}
 }
